4539b88418556891c85dc273e1c990e846e583ba
Felipe <felipe.silvar85@gmail.com>
2025-09-12 13:52:42 -0300

    se agrega dto

--- src/main/java/com/springboot/app/controllers/UserRestController.java
+++ src/main/java/com/springboot/app/controllers/UserRestController.java
@@ -1,6 +1,7 @@
 package com.springboot.app.controllers;
 
 import com.springboot.app.models.User;
+import com.springboot.app.models.dto.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,26 @@ import java.util.Map;
 @RequestMapping("/api")
 public class UserRestController {
 
-//  @RequestMapping(path="/details", method = requestMethod.GET)
-    @GetMapping("/details")
-    public Map<String, Object> details() {
-        User user = new User("Felipe", "Silva");
-        Map<String, Object> body = new HashMap<>();
+//    @RequestMapping(path="/details", method = requestMethod.GET) // otra forma de hacerlo
+//    @GetMapping("/details")
+//    public Map<String, Object> details() {
+//        User user = new User("Felipe", "Silva");
+//        Map<String, Object> body = new HashMap<>();
+//
+//        body.put("title", "Hola Mundo Spring Boot con Thymeleaf");
+//        body.put("user", user);
+//        return body;
+//    }
 
-        body.put("title", "Hola Mundo Spring Boot con Thymeleaf");
-        body.put("user", user);
-        return body;
+    @GetMapping(path = "/details-dto")
+    public UserDto details() {
+        UserDto userDto = new UserDto();
+        User user = new User("Felipe", "Silva");
+
+        userDto.setTitle("Hola Mundo Spring Boot con Thymeleaf");
+        userDto.setUser(user);
+
+        return userDto;
     }
 
 }
